4def887f3a7a001cf0539d013cb62402f23a6609
Athar Bakth <atharbakth@gmail.com>
2019-06-13 12:46:36 -0400

    Add files via upload
    Added counters for 8 different amino acids.

---
+++ codon3.0py.py
@@ -0,0 +1,162 @@
+import pprint
+
+seq_a='ACGAACGAAAATGGT'
+seq_b='ACTCCACTGCCCGGA'
+
+length_a = len(seq_a)
+length_b = len(seq_b)
+
+#made to check if seq a and b are the same length
+if (length_a == length_b):
+    print('Loading... ')
+elif (len(seq_a) != len(seq_b)):
+    print('***Go cry in a corner cause program will not work, The sequences are not the same Length***')
+    quit()
+
+#breaks string into 3 equal parts instead of one long string
+seq_a_codon=[seq_a[i:i+3] for i in range(0, len(seq_a), 3)]
+seq_b_codon=[seq_b[i:i+3] for i in range(0, len(seq_b), 3)]
+
+
+#made to check if it is possible to break the sequences into 3 seperate pieces equally
+for stuff in seq_a_codon:
+    if len(stuff) != 3:
+        print('***Exit the program and throw your computer at the wall because of the disappointment you\'ve brought to your parents: Error A***')
+        quit()
+for stuff in seq_b_codon:
+    if len(stuff) != 3:
+        print('***Exit the program and throw your computer at the wall because of the disappointment you\'ve brought to your parents: Error B***')
+        quit()
+#print('Codon group for string A is ' +str(seq_a_codon))
+#print('Codon group for string B is ' +str(seq_b_codon))
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+start =0
+difference=0
+similar=0
+n=0
+different_codons=[]
+#assume that all the sequences are equal in length 
+while start < (len(seq_a_codon) and len(seq_b_codon)):
+    #puts different group into tuples to make sure it is immutible in the program
+    if seq_a_codon[n] != seq_b_codon[n]:
+        difference +=1
+        a=seq_a_codon[n]
+        b=seq_b_codon[n]
+        differences=(a,b)
+        different_codons.append(differences)
+        #appends tuples to list
+    elif seq_a_codon[n] == seq_b_codon[n]:
+        similar +=1
+        #delete chunk on similar before moving to excel
+    else:
+        pass
+    start +=1
+    n+=1
+
+
+i=0
+thirdbaseA = []
+thirdbaseB = []
+while i < len(different_codons):
+    A,B = different_codons[i]
+    #hereistheproblem
+    Alist = list(A)
+    Blist = list(B)
+    thirdbaseA.append(Alist)
+    thirdbaseB.append(Blist)
+    i+=1
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+#makes pairs mutable by inserting it into a list
+
+if not thirdbaseA:
+    print('There is no similarities seq A')
+    quit()
+if not thirdbaseB:
+    print('There is no similaarities seq B')
+    quit()
+    
+counter=0
+n=0
+pairs =[]
+#algorithm that determines if 3rd base are not equal and if so are stored into pairs with a tuple
+while counter < (len(thirdbaseA)):
+    if thirdbaseA[n][2] == thirdbaseB[n][2]:
+        n+=1
+        counter+=1
+    elif thirdbaseA[n][2] != thirdbaseB[n][2]:
+        if (thirdbaseA[n][0] == thirdbaseB[n][0]) and (thirdbaseA[n][1] == thirdbaseB[n][1]):
+            seq_a_base = thirdbaseA[n]
+            seq_b_base = thirdbaseB[n]
+            compare =(seq_a_base,seq_b_base)
+            pairs.append(compare)
+            counter+=1
+            n+=1
+        else:
+            counter+=1
+            n+=1
+        
+if not pairs:
+    print('**The first two bases for codons are the same**')
+    quit()
+print('____________________')
+
+#this part will count the changes
+TtoC=0
+CtoT=0
+GtoA=0
+AtoG=0
+
+
+def tuplereader(pair):
+    Glycine = 0
+    Alanine = 0
+    Valine = 0
+    Threonine = 0
+    Arginine = 0
+    Proline = 0
+    Leucine = 0
+    Serine = 0
+    for i in pair:
+        if (i[0][0] == 'A') and (i[0][1] == 'C'):
+            Threonine +=1
+        elif (i[0][0] == 'G') and (i[0][1] == 'T'):
+            Valine +=1
+        elif (i[0][0] == 'G') and (i[0][1] == 'C'):
+            Alanine +=1
+        elif (i[0][0] == 'G') and (i[0][1] == 'G'):
+            Glycine +=1
+        elif (i[0][0] == 'T') and (i[0][1] == 'C'):
+            Serine +=1
+        elif (i[0][0] == 'C') and (i[0][1] == 'T'):
+            Leucine +=1
+        elif (i[0][0] == 'C') and (i[0][1] == 'C'):
+            Proline +=1
+        elif (i[0][0] == 'C') and (i[0][1] == 'G'):
+            Arginine +=1
+        else:
+            continue
+    for i in pair:
+        if (i[1][0] == 'A') and (i[1][1] == 'C'):
+            Threonine +=1
+        elif (i[1][0] == 'G') and (i[1][1] == 'T'):
+            Valine +=1
+        elif (i[1][0] == 'G') and (i[1][1] == 'C'):
+            Alanine +=1
+        elif (i[1][0] == 'G') and (i[1][1] == 'G'):
+            Glycine +=1
+        elif (i[1][0] == 'T') and (i[1][1] == 'C'):
+            Serine +=1
+        elif (i[1][0] == 'C') and (i[1][1] == 'T'):
+            Leucine +=1
+        elif (i[1][0] == 'C') and (i[1][1] == 'C'):
+            Proline +=1
+        elif (i[1][0] == 'C') and (i[1][1] == 'G'):
+            Arginine +=1
+        else:
+            continue
+
+    print('Glycine : %s, \nAlanine: %s, \nValine: %s, \nThreonine: %s, \nArginine: %s, \nProline: %s, \nLeucine: %s, \nSerine: %s' %(Glycine, Alanine, Valine, Threonine, Arginine, Proline, Leucine, Serine))
+tuplereader(pairs)
+pprint.pprint(pairs)
